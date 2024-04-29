de16934cf1d0d50ddbc06451bdad748dc2f23edb
root <root@DESKTOP-ICK7MPV>
2024-04-29 16:23:47 +0300

    introduction of an endpoint handling put requests

--- myapi.py
+++ myapi.py
@@ -18,6 +18,12 @@ class Student(BaseModel):
     age: int
     year: str
 
+#class to handle update requests
+class UpdateStudent(BaseModel):
+    name: Optional[str] = None
+    age: Optional[int] = None
+    year: Optional[str] = None
+
 @app.get("/")
 def index():
     return {"message": "Hello, World!"}
@@ -39,10 +45,32 @@ def get_student(name: Optional[str] = None):
         
     return {"Data": "Not found"}
 
-#request body
+#request body with post
 @app.post("/create-student/{student_id}")
 def create_studwnt(student_id: int, student: Student):
     if student_id in students:
         return {"Error": "Student exists"}
     students[student_id] = student
     return students[student_id]
+
+#put request
+@app.put("/update-student/{student_id}")
+def updatestudent(student_id: int, student: UpdateStudent):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    if student.name != None:
+        students[student_id].name = student.name
+    if student.age != None:
+        students[student_id].age = student.age
+    if student.year != None:
+        students[student_id].year = student.year
+
+    return students[student_id]
+
+#delete request
+@app.delete("/delete-student/{student_id}")
+def delete_student(student_id: int):
+    if student_id not in students:
+        return {"Error": "Student does not exist"}
+    del students[student_id]
+    return {"Message": "Student deleted successfully"}
